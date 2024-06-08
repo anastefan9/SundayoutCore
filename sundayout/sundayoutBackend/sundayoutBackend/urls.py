d3b956088254c3e2ddc0831cd68045f6b895c6ba
anastefan9 <ana.stefan1000@gmail.com>
2024-06-08 21:34:48 +0300

    Added sundayout backend app

---
+++ sundayout/sundayoutBackend/sundayoutBackend/urls.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+   re_path('login', views.login),
+   re_path('signup', views.signup),
+   re_path('test_token', views.test_token)
+]
+
+# we want to map those views to certain routes with our urls
+
+# now we have an api for login, signup, and test_token
+
+# whenever we make a post request to any of the POST requests from the views, it will return us an empty response object
